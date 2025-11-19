f14c8de24ae195e23d99c1e8cc379fe37b4e1b06
Serhii Kroshka <pm012@ukr.net>
2025-11-19 13:09:29 +0200

    get_completion method implemented for custom_client

--- task/clients/custom_client.py
+++ task/clients/custom_client.py
@@ -1,7 +1,9 @@
 import json
+import os
 import aiohttp
 import requests
 
+from dotenv import load_dotenv
 from task.clients.base import BaseClient
 from task.constants import DIAL_ENDPOINT
 from task.models.message import Message
@@ -14,24 +16,46 @@ class CustomDialClient(BaseClient):
 
     def __init__(self, deployment_name: str):
         super().__init__(deployment_name)
-        self._endpoint = DIAL_ENDPOINT + f"/openai/deployments/{deployment_name}/chat/completions"
+        self._api_key = os.getenv("DIAL_API_KEY", "")
+        self._base_url = os.getenv("DIAL_API_BASE", "")
+        self._endpoint = self._base_url + f"/openai/deployments/{deployment_name}/chat/completions"
 
     def get_completion(self, messages: list[Message]) -> Message:
-        #TODO:
+       
         # Take a look at README.md of how the request and regular response are looks like!
         # 1. Create headers dict with api-key and Content-Type
+        headers = {
+            "api-key": self._api_key,
+            "Content-Type": "application/json"
+        }
         # 2. Create request_data dictionary with:
         #   - "messages": convert messages list to dict format using msg.to_dict() for each message
+        request_data = {
+            "messages": [message.to_dict() for message in messages]
+        }
         # 3. Make POST request using requests.post() with:
         #   - URL: self._endpoint
         #   - headers: headers from step 1
         #   - json: request_data from step 2
+        response = requests.post(
+            url=self._endpoint,
+            headers=headers,
+            json=request_data
+        )
         # 4. Get content from response, print it and return message with assistant role and content
+        if response.status_code == 200:
+            response_data = response.json()
+            if choices := response_data.get("choices", []):
+                if choices:
+                    content = choices[0].get("message",{}).get("content")                    
+                    print(content)
+                    return Message(role=Role.AI, content=content)
         # 5. If status code != 200 then raise Exception with format: f"HTTP {response.status_code}: {response.text}"
-        raise NotImplementedError
+        else:
+            raise Exception(f"HTTP {response.status_code}: {response.text}")
 
     async def stream_completion(self, messages: list[Message]) -> Message:
-        #TODO:
+        
         # Take a look at README.md of how the request and streamed response chunks are looks like!
         # 1. Create headers dict with api-key and Content-Type
         # 2. Create request_data dictionary with:
